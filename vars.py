629282715b7ea5f8049e88ae849ff03ac2fc3fa3
retornam <etornam.raymond@gmail.com>
2011-05-03 16:30:27 -0700

    edits to vars so it can play nice with jenkins

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 class ConnectionParameters:
     server ="localhost"
     port = 4444
-    browser = "firefox"
+    browser = "Firefox4b;en-us;MacOSX6"
     baseurl = "https://developer-stage9.mozilla.org/en-US/"
     page_load_timeout = 120000
