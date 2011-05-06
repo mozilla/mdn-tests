31a5e62f77c4664f22a973e29ecd700d3d500f0f
retornam <etornam.raymond@gmail.com>
2011-05-05 17:02:35 -0700

    making changes to vars for production

--- vars.py
+++ vars.py
@@ -2,5 +2,5 @@ class ConnectionParameters:
     server ="localhost"
     port = 4444
     browser = "firefox"
-    baseurl = "https://developer-stage9.mozilla.org/en-US/"
+    baseurl = "https://developer.mozilla.org/en-US/"
     page_load_timeout = 120000
