484db03387e5e1889c36fd69f3f7d95cd30a2fd7
retornam <etornam.raymond@gmail.com>
2011-05-05 19:54:23 -0700

    Making corrections to vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 class ConnectionParameters:
     server ="localhost"
     port = 4444
-    browser = "irefox4b;en-us;MacOSX6"
+    browser = "Firefox4b;en-us;MacOSX6"
     baseurl = "https://developer.mozilla.org/en-US/"
     page_load_timeout = 120000
