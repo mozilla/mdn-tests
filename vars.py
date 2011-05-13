d587251751e493c736d7aa16824659f71b619f07
retornam <etornam.raymond@gmail.com>
2011-05-13 10:08:12 -0700

    made change to run on Firefox 4

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 class ConnectionParameters:
     server ="localhost"
     port = 4444
-    browser = "Firefox4b;en-us;MacOSX6"
+    browser = "Firefox-4;en-us;MacOSX6"
     baseurl = "https://developer.mozilla.org/en-US/"
     page_load_timeout = 120000
