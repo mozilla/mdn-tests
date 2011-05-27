e406e4ce09fbea316b9550c62553a7c9e92f2815
retornam <etornam.raymond@gmail.com>
2011-05-27 08:55:36 -0700

    making changes for conftest

--- test_tags.py
+++ test_tags.py
@@ -1,32 +1,17 @@
-import unittest
 from selenium import selenium
-from demo_page import DemoPage
+from unittestzero import Assert
 from test_homepage import TestHomepage
-from vars import ConnectionParameters
+from demo_page import DemoPage
 
 
-class TestTags(unittest.TestCase):
-    
-    def setUp(self):
-        self.selenium = selenium(ConnectionParameters.server,\
-        ConnectionParameters.port,ConnectionParameters.browser,\
-        ConnectionParameters.baseurl)
-        self.selenium.start()
-        self.selenium.set_timeout(ConnectionParameters.page_load_timeout)
-        
-    def tearDown(self):
-        self.selenium.stop()
-        
-    def test_all_tags(self):
-        demo_pg = DemoPage(self.selenium)
-        demo_pg.go_to_page
+
+class TestTags:
+            
+    def test_all_tags(self,testsetup):
+        self.selenium = testsetup.selenium
+        demo_pg = DemoPage(testsetup)
+        demo_pg.open("/demos")
         links = demo_pg.get_tag_links
         for keys, values in sorted(links.items()):
             demo_pg.open(links[keys])
-
-            
-            
-            
-    
-if __name__ == "__main__":
-    unittest.main()    
+  
