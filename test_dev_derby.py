90c4157be28a97fb2007f3720bda8e6cdc8521ad
retornam <etornam.raymond@gmail.com>
2011-07-19 17:03:16 -0700

    adding xfail for production as  there are no derby winners there'

--- test_dev_derby.py
+++ test_dev_derby.py
@@ -2,6 +2,7 @@ from selenium import selenium
 from unittestzero import Assert
 from demo_page import DemoPage
 from derby_page import DerbyPage
+xfail = pytest.mark.xfail
 
 
 class TestDevDerbyPage:
@@ -16,7 +17,7 @@ class TestDevDerbyPage:
         Assert.true(derby_pg.footer_privacy_link)
         Assert.true(derby_pg.footer_help_link)
         
-        
+    @xfail(reason="No derby winners on production yet")
     def test_derby_page(self,testsetup):
         derby_pg = DerbyPage(testsetup)
         derby_pg.open("en-US/demos/devderby")
