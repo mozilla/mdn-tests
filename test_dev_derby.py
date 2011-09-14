a7d36a3a03dd0c7908f0645e5e1c454c8ff20903
retornam <etornam.raymond@gmail.com>
2011-09-14 12:49:04 -0700

    Adding changes for py.test_mozwebqa

--- test_dev_derby.py
+++ test_dev_derby.py
@@ -8,8 +8,8 @@ xfail = pytest.mark.xfail
 
 class TestDevDerbyPage:
 
-    def test_are_footer_links_visible(self, testsetup):
-        derby_pg = DemoPage(testsetup)
+    def test_are_footer_links_visible(self, mozwebqa):
+        derby_pg = DemoPage(mozwebqa)
         derby_pg.open("en-US/demos/devderby")
         Assert.true(derby_pg.is_footer_img_visible)
         Assert.true(derby_pg.is_footer_bar_feedback_link_visible)
@@ -19,8 +19,8 @@ class TestDevDerbyPage:
         Assert.true(derby_pg.is_footer_help_link_visible)
 
    ## @xfail(reason="No derby winners on production yet")
-    def test_derby_links_visible(self, testsetup):
-        derby_pg = DerbyPage(testsetup)
+    def test_derby_links_visible(self, mozwebqa):
+        derby_pg = DerbyPage(mozwebqa)
         derby_pg.go_to_derby_page()
         Assert.true(derby_pg.is_home_link_visible)
         Assert.true(derby_pg.is_challenges_link_visible)
@@ -39,15 +39,15 @@ class TestDevDerbyPage:
         Assert.true(derby_pg.is_prizes_image_visible)
         Assert.equal(derby_pg.get_prizes_text, "Prizes")
 
-    def test_judge_images_visible(self, testsetup):
-        derby_pg = DerbyPage(testsetup)
+    def test_judge_images_visible(self, mozwebqa):
+        derby_pg = DerbyPage(mozwebqa)
         derby_pg.go_to_derby_page()
         number_of_judges = derby_pg.get_number_of_judges
         for i in range(1, number_of_judges):
             Assert.true(derby_pg.is_judge_photo_visible(i))
 
-    def test_are_previous_challenges_present(self, testsetup):
-        derby_pg = DerbyPage(testsetup)
+    def test_are_previous_challenges_present(self, mozwebqa):
+        derby_pg = DerbyPage(mozwebqa)
         derby_pg.go_to_derby_page()
         number_of_challenges = derby_pg.get_number_of_previous_challenges
         for i in range(1, number_of_challenges):
--- test_homepage.py
+++ test_homepage.py
@@ -5,8 +5,8 @@ from demo_page import DemoPage
 
 class TestHomepage:
 
-    def test_header_links(self, testsetup):
-        demo_pg = DemoPage(testsetup)
+    def test_header_links(self, mozwebqa):
+        demo_pg = DemoPage(mozwebqa)
         demo_pg.go_to_demo_page()
         Assert.true(demo_pg.is_topics_link_visible)
         Assert.true(demo_pg.is_docs_link_visible)
@@ -25,8 +25,8 @@ class TestHomepage:
         Assert.true(demo_pg.is_demo_studio_link_visible)
         Assert.true(demo_pg.is_learn_more_link_visible)
 
-    def test_footer_links(self, testsetup):
-        demo_pg = DemoPage(testsetup)
+    def test_footer_links(self, mozwebqa):
+        demo_pg = DemoPage(mozwebqa)
         demo_pg.go_to_demo_page()
         Assert.true(demo_pg.is_footer_img_visible)
         Assert.true(demo_pg.is_footer_rss_link_visible)
@@ -36,15 +36,15 @@ class TestHomepage:
         Assert.true(demo_pg.is_footer_privacy_link_visible)
         Assert.true(demo_pg.is_footer_help_link_visible)
 
-    def test_demo_title(self, testsetup):
-        demo_pg = DemoPage(testsetup)
+    def test_demo_title(self, mozwebqa):
+        demo_pg = DemoPage(mozwebqa)
         demo_pg.open("/demos")
         totalDemoTitles = demo_pg.get_demo_title_count()
         for i in range(1, totalDemoTitles+1):
             Assert.true(demo_pg.is_demo_title_visible(i))
 
-    def test_demo_image(self, testsetup):
-        demo_pg = DemoPage(testsetup)
+    def test_demo_image(self, mozwebqa):
+        demo_pg = DemoPage(mozwebqa)
         demo_pg.open("/demos")
         totalDemoImages = demo_pg.get_demo_image_count()
         for i in range(1, totalDemoImages+1):
