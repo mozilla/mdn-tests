395faa145566670240aa883ca067c51afe59eb65
retornam <etornam.raymond@gmail.com>
2011-09-21 18:00:39 -0700

    changing testsetup to mozwebqa

--- test_learn_page.py
+++ test_learn_page.py
@@ -43,8 +43,8 @@ from learn_page import LearnPage
 
 class TestLearnPage:
 
-    def test_are_footer_links_visible(self, testsetup):
-        learn_pg = DemoPage(testsetup)
+    def test_are_footer_links_visible(self, mozwebqa):
+        learn_pg = DemoPage(mozwebqa)
         learn_pg.open("en-US/learn")
         Assert.true(learn_pg.is_footer_img_visible)
         Assert.true(learn_pg.is_footer_bar_feedback_link_visible)
@@ -53,8 +53,8 @@ class TestLearnPage:
         Assert.true(learn_pg.is_footer_privacy_link_visible)
         Assert.true(learn_pg.is_footer_help_link_visible)
 
-    def test_header_links_visible(self, testsetup):
-        learn_pg = DemoPage(testsetup)
+    def test_header_links_visible(self, mozwebqa):
+        learn_pg = DemoPage(mozwebqa)
         learn_pg.open("en-US/learn")
         Assert.true(learn_pg.is_topics_link_visible)
         Assert.true(learn_pg.is_docs_link_visible)
@@ -70,8 +70,8 @@ class TestLearnPage:
         Assert.true(learn_pg.is_login_link_visible)
         Assert.true(learn_pg.is_search_mdn_link_visible)
 
-    def test_page_elements_are_visible(self, testsetup):
-        learn_pg = LearnPage(testsetup)
+    def test_page_elements_are_visible(self, mozwebqa):
+        learn_pg = LearnPage(mozwebqa)
         learn_pg.go_to_learn_page()
         Assert.true(learn_pg.is_make_web_better_visible)
         Assert.true(learn_pg.is_html_locator_visible)
